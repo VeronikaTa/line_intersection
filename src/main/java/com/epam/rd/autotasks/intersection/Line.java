a67bf664074f7af92d7c1d39e1262bb3825cb781
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-24 17:38:23 +0300

    edited class Line

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,32 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
-
+    private int k;
+    private int b;
     public Line(int k, int b) {
+    this.k = k;
+    this.b = b;
 
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        int x_intercept = 0;
+        int y_intercept = 0;
+        Point intersection;
+
+//        int xFirst = (0 - this.b)/this.k;
+//        int yFirst = this.b;
+//        int xOther = (0 - other.b)/other.k;
+//        int yOther = other.b;
+        if((other.k - this.k) != 0){
+            x_intercept = (this.b - other.b)/(other.k - this.k);
+            y_intercept = (other.k * this.b - this.k * other.b) / (other.k - this.k);
+            intersection = new Point(x_intercept,y_intercept);
+        }
+        else{
+            intersection = null;
+        }
+        return intersection;
     }
 
 }
