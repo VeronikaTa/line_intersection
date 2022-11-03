6ef158f8eaf6f15f1df81b7b68984db9b6afa42a
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-11-03 18:40:47 +0200

    tasks completed

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,5 +1,5 @@
-package com.epam.rd.autotasks.intersection;
-
+package com.epam.training.student_veronika_tarasova.line_intersection.src.main.java.com.epam.rd.autotasks.intersection;
+import com.epam.rd.autotasks.intersection.Point;
 public class Line {
     private int k;
     private int b;
@@ -14,10 +14,6 @@ public class Line {
         int y_intercept = 0;
         Point intersection;
 
-//        int xFirst = (0 - this.b)/this.k;
-//        int yFirst = this.b;
-//        int xOther = (0 - other.b)/other.k;
-//        int yOther = other.b;
         if((other.k - this.k) != 0){
             x_intercept = (this.b - other.b)/(other.k - this.k);
             y_intercept = (other.k * this.b - this.k * other.b) / (other.k - this.k);
